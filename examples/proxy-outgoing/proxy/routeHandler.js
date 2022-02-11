73dee21387462f51b3f2eacb784c15ac1e17451b
Nox <the0neWhoKnocks@users.noreply.github.com>
2022-02-11 11:49:07 -0800

    Added POST example for proxy-outgoing

--- examples/proxy-outgoing/proxy/routeHandler.js
+++ examples/proxy-outgoing/proxy/routeHandler.js
@@ -1,14 +1,26 @@
 module.exports = function routeHandler(req, res) {
-  const domain = req.headers.host;
-  const url = `${!!req.socket.ssl ? 'https' : 'http'}://${domain}${req.originalUrl}`;
+  const { body, headers, method, originalUrl, socket } = req;
+  const domain = headers.host;
+  const url = `${!!socket.ssl ? 'https' : 'http'}://${domain}${originalUrl}`;
   
   console.log(`[PROXY] Request for: "${url}"`);
   
   if (
     domain === 'api.icndb.com'
+    || domain === 'jsonplaceholder.typicode.com'
     || domain === 'rickandmortyapi.com'
   ) {
-    return res.json({ mock: `for "${url}"` });
+    let extraData = {};
+    
+    if (method === 'POST') {
+      extraData = { postBody: body };
+    }
+    
+    return res.json({
+      method,
+      mock: `for "${url}"`,
+      ...extraData,
+    });
   }
   
   res.send('');
--- examples/proxy-outgoing/proxy/server.js
+++ examples/proxy-outgoing/proxy/server.js
@@ -1,11 +1,17 @@
 const { readFileSync } = require('fs');
 const http = require('http');
 const https = require('https');
+const bodyParser = require('body-parser');
 const express = require('express');
 const requireCurrent = require('./requireCurrent');
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+// handle requests
 app.use('*', (...args) => requireCurrent('./routeHandler')(...args));
 
 function portHandler(port) {
