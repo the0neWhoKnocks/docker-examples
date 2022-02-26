ff1ca1a4d0fb1de2dbdc3217fd9997374a264091
Nox <the0neWhoKnocks@users.noreply.github.com>
2022-02-26 11:12:17 -0800

    Proxy API additions and fixed headers already sent issue
    - There may be cases where a User wants to use CLI colors, or require un-cached modules so I added in `color` and `requireCurrent`. They were available, but linters complain since the modules are only available in the Container.
    - The `res.headersSent` prop wasn't always accurate and I was getting random 'headers already sent' errors, along with passthrough messages. So I've switched to the `next` middleware way of doing things to ensure a blank passthrough response is only sent when a Dev specifies.

--- examples/proxy-outgoing/proxy/src/routeHandler.js
+++ examples/proxy-outgoing/proxy/src/routeHandler.js
@@ -5,20 +5,31 @@ const { color, formatReqData } = require('./shared');
 module.exports = async function routeHandler(req, res) {
   const formattedReq = formatReqData(req);
   const { body, domain, fullURL, method } = formattedReq;
+  let matchFound = true;
   
   try {
     const matcher = requireCurrent('./matcher');
     
     console.log(`${color.block.good('PROXY')} Request for: ${color.text.info(fullURL)}`);
     
-    await matcher({ cacheResp, req: formattedReq, res });
+    await matcher({
+      cacheResp,
+      color,
+      next: (err) => {
+        matchFound = false;
+        if (err) throw err;
+      },
+      req: formattedReq,
+      requireCurrent,
+      res,
+    });
   }
   catch (err) {
     console.log(`${color.block.bad('ERROR')}\n${color.text.bad(err.stack)}`);
   }
   
-  if (!res.headersSent) {
-    console.log(`${color.block.comment('PASSTHROUGH')} Request for: ${color.text.info(fullURL)}`);
+  if (!matchFound) {
+    console.log(`${color.block.warn('PASSTHROUGH')} Request for: ${color.text.info(fullURL)}`);
     res.send('');
   }
 }
